7be2581a3aa19fcc208afaf014ec1a33911f76fc
lisihang <1029076022@qq.com>
2018-11-27 23:35:23 +0800

    crawler for lectures

--- crawler/lecture_crawler.py
+++ crawler/lecture_crawler.py
@@ -14,38 +14,48 @@ head = {
 	'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'
 }
 
-url = "http://lecture.pku.edu.cn/index_new.php?page="
+url = "http://resource.pku.edu.cn/index.php?r=lecturepre/view&id="
 
 titles = []
 speakers = []
-times = []
 places = []
+dates = []
+times = []
+faculties = []
+schools = []
+labels = []
 
 def crawler():
-	today = time.strftime('%Y.%m.%d',time.localtime(time.time()))
-	for i in range(1000):
-		f = False
-		r = requests.get(url + str(i+1), headers=head)
+	for i in range(1, 500, 2):
+		r = requests.get(url+str(i), headers=head)
 		r.encoding = "utf-8"
 		soup = BeautifulSoup(r.text, "html.parser")
-		for div in soup.find_all("div"):
-			if div.get("class") == ["infortext"]:
-				t = str(div).split("<br/>")
-				title = re.findall(r"\"_blank\">(.+?)</a>", t[0])[0]
-				speaker = re.findall(r"主讲人： (.+?) $", t[1])[0]
-				tim = re.findall(r"时    间：(.+?)      ", t[2])[0]
-				place = re.findall(r"地点：：(.+?) </div>", t[2])[0]
-
-				day = tim.split(" ")[0].replace("年", ".").replace("月", ".").replace("日", "")
-				if day < today:
-					f = True
+		for sec in soup.find_all("section"):
+			if(sec.get("class") == ["pull-left", "lec-content", "bg-white"]):
+				title = sec.find("h3").get_text()
+				if(title == " "):
 					break
+				title.strip()
+				div = sec.find_all("div", attrs={"class": "brief"})
+				speaker = div[0].get_text()
+				place = div[1].get_text()
+				t = div[2].get_text().split(" ")
+				date = t[0]
+				time = t[1]
+				t = div[7].get_text().split(" / ")
+				faculty = t[0]
+				school = t[1]
+				label = div[8].get_text().strip("\n")
+
 				titles.append(title)
 				speakers.append(speaker)
-				times.append(tim)
 				places.append(place)
-		if f:
-			break
-	return titles, speakers, times, places
+				dates.append(date)
+				times.append(time)
+				faculties.append(faculty)
+				schools.append(school)
+				labels.append(label)
+
+	return titles, speakers, places, dates, times, faculties, schools, labels
 
 crawler()
