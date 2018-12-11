3b0d620010f403b8fc934abc3a8cac976f286802
lisihang <1029076022@qq.com>
2018-12-11 22:00:59 +0800

    crawler for hole

--- crawler/hole_crawler.py
+++ crawler/hole_crawler.py
@@ -8,25 +8,28 @@ import json
 from bs4 import BeautifulSoup
 
 head = {
-	"Host": "www.pkuhelper.com:10301",
+	"Host": "www.pkuhelper.com",
 	"Accept": "*/*",
 	"Accept-Language": "zh-Hans-CN;q=1",
 	"Connection": "keep-alive",
 	"Accept-Encoding": "gzip, deflate",
 	"User-Agent": "PKU Helper/2.3.8 (iPhone; iOS 12.1; Scale/3.00)"
 }
-url = "http://www.pkuhelper.com/pkuhelper/../services/pkuhole/api.php"
+url = "http://162.105.205.61/services/pkuhole/api.php"
+pids = []
+texts = []
+replys = []
+likenums = []
 def crawler():
-	para = {"action": "getlist", "p": "1"}
-	r = requests.get(url, headers=head, params=para)
-	data = json.loads(r.text)["data"]
-	pids = []
-	texts = []
-	replys = []
-	likenums = []
-	for t in data:
-		pids.append(int(t["pid"]))
-		texts.append(t["text"])
-		replys.append(int(t["reply"]))
-		likenums.append(int(t["likenum"]))
+	for i in range(5):
+		para = {"action": "getlist", "p": str(i+1)}
+		r = requests.get(url, headers=head, params=para)
+		data = json.loads(r.text)["data"]
+		for t in data:
+			if int(t["pid"]) in pids:
+				continue
+			pids.append(int(t["pid"]))
+			texts.append(t["text"])
+			replys.append(int(t["reply"]))
+			likenums.append(int(t["likenum"]))
 	return pids, texts, replys, likenums
