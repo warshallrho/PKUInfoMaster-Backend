6b455ec2fb6f76ed03e8eb1bea85058463a15733
lisihang <1029076022@qq.com>
2018-12-22 14:47:39 +0800

    update classroom crawler

--- crawler/classroom_crawler.py
+++ crawler/classroom_crawler.py
@@ -5,6 +5,8 @@ classrooms = ["一教", "二教", "三教", "四教", "理教", "文史", "电�
 
 para = {"buildingName": "", "time": "今天"}
 
+cookies={"JSESSIONID": "F8AE754B35FFBA0BCF43068DE87D6BA4", "UM_distinctid": "1663a9dc24b0-043f5a1930fc8c-8383268-144000-1663a9dc24c7da", "_astraeus_session": "WXFLVkd2N0s5THBQUldzYWpUOU5qZWlTSDh6YVpvQVBhVTBHc0wrWFVBTEMrTzJLTXk4QXllOHJNN2VzZUJ6U3d2TzNLaWgzVUh3TkJuMXhBQzVycklNdU4ybGkzNGlYR045aldCb0JIcjJOdC9PVjZYRFhVT1lmZ21kSWVsZjAzR2QrQkxUZ0lvQlFvenlkM2ZsdWYxVDQ2aTJMTVlneDI4U2pxRmVaVWtSR2J2dTNQcjJWODZtVGt5b2hwZ1Q0NGZZMU9yc05XVVA2Q3VHWERPN1pqc3BDNW0rcGQrb0dLb0dBcWRmakF6YXowQllOOWI0SURtWmFleEQvRHNoSnc2VFAwM01iUG5yZmxzY3EyUVpJVDJQRnQrT0ZUR0lmYzUrdXRZalpMSTNoVDRIUDJ2VjVNQk5JeHo2ZXh1NW80SzNvbHF4ZDFta2pxano3TGhYMVhNZHA0VkNNdHVtM2FIcUtIWXdpMFNlcXBsL2s4V2Q4RFFudGJmLzhDWEhmbGJwS0ZJZy9zNHZFWTN3OVRVMlNZYUJ4RzgrdG5lWGNBTkJMQ1FoSWlyRkk1a215Yi9TU0psVUd1ZlQ5TVduZS0tN0NpdzNVQXdoVFd2enJ3aFJGNGJCUT09--652de24fb5ebcffdd6eafe526733ba00c773030a", "_ga": "GA1.3.1746632523.1524661182"}
+
 buildings = []
 rooms = []
 capacitys = []
@@ -13,7 +15,8 @@ infos = []
 def crawler():
 	for building in classrooms:
 		para["buildingName"] = building
-		r = requests.get("https://portal.pku.edu.cn/portal2017/publicsearch/classroom/retrClassRoomFree.do", params=para)
+		r = requests.get("https://portal.w.pku.edu.cn/portal2017/publicsearch/classroom/retrClassRoomFree.do", params=para, cookies=cookies)
+		print(r.text)
 		array = json.loads(r.text, strict=False)["rows"]
 		for room in array:
 			buildings.append(building)
@@ -27,3 +30,4 @@ def crawler():
 					s = s + "1"
 			infos.append(s)
 	return buildings, rooms, capacitys, infos
+crawler()
